c75762092a144ea247043bf0fcbff3bd1bd106b5
Vishal Borana <vishal2develop@gmail.com>
2024-04-10 00:13:09 +0530

    configured amplify & setup in our app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-// import HomeScreen from './src/screens/HomeScreen/HomeScreen';
-// import CommentScreen from './src/screens/CommentScreen/CommentScreen';
-// import ProfileScreen from './src/screens/ProfileScreen/ProfileScreen';
-// import EditProfileScreen from './src/screens/EditProfileScreen/EditProfileScreen';
-// import PostUploadScreen from './src/screens/PostUploadScreen';
-
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigation from './src/navigation';
+import {Amplify} from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
 
 const App = () => {
   return (
