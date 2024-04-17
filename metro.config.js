24df69f208cdd5b3960005d51795a97d77c1740a
Vishal Borana <vishal2develop@gmail.com>
2024-04-17 23:02:50 +0530

    Fixed Amlify Duplicated files or mocks issue

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,10 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const {getDefaultConfig} = require('expo/metro-config');
+const exclusionList = require('metro-config/src/defaults/exclusionList');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const config = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+config.resolver.blacklistRE = exclusionList([
+  /amplify\/#current-cloud-backend\/.*/,
+]);
+
+module.exports = config;
