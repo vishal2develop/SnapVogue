1be3f64e60c7e8096cc83aa52d3ff2bbfd22e8d9
Vishal Borana <vishal2develop@gmail.com>
2024-05-29 18:37:36 +0530

    - Issue Resolved -  Posts not visible on profile screen

--- src/components/FeedGridView/FeedGridItem.tsx
+++ src/components/FeedGridView/FeedGridItem.tsx
@@ -1,17 +1,50 @@
 import {View, Image} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import colors from '../../theme/color';
 import {Post} from '../../API';
 
+import {getUrl, GetUrlOutput} from '@aws-amplify/storage';
+import VideoPlayer from '../VideoPlayer';
 interface IFeedGridItem {
   post: Post;
 }
 
 const FeedGridItem = ({post}: IFeedGridItem) => {
+  const [imageUri, setImageUri] = useState<string | null | URL>(null);
+  const [imagesUri, setImagesUri] = useState<
+    string[] | null | GetUrlOutput[] | URL[]
+  >(null);
+  const [videoUri, setVideoUri] = useState<string | null | GetUrlOutput>(null);
+
+  useEffect(() => {
+    downloadMedia();
+  }, []);
+
+  const downloadMedia = async () => {
+    if (post.image) {
+      // download the image
+      const uri = (await getUrl({key: post.image})).url;
+
+      setImageUri(uri.href);
+    } else if (post.images) {
+      const uriObjs = await Promise.all(
+        post.images.map(img => getUrl({key: img})),
+      );
+      const uris = uriObjs.map(uri => uri.url);
+      setImagesUri(uris);
+    } else if (post.video) {
+      const uri = (await getUrl({key: post.video})).url;
+      setVideoUri(uri.href);
+    }
+  };
+
   return (
     <View style={{flex: 1, padding: 1, aspectRatio: 1, maxWidth: '33.33%'}}>
-      <Image source={{uri: post.image || post.images?.[0]}} style={{flex: 1}} />
+      <Image
+        source={{uri: imageUri || imagesUri?.[0]?.href}}
+        style={{flex: 1}}
+      />
       {/* To display the collections icon on the post, in case the post contains multiple photos */}
       {post.images && (
         <MaterialIcons
@@ -21,6 +54,7 @@ const FeedGridItem = ({post}: IFeedGridItem) => {
           style={{position: 'absolute', top: 5, right: 5}}
         />
       )}
+      {videoUri && <VideoPlayer uri={videoUri} paused />}
     </View>
   );
 };
